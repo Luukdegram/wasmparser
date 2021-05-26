bd1fd3f533485c65e909b0c4bfdd6ef63131095f
Luuk de Gram <Luukdegram@users.noreply.github.com>
2021-05-26 16:42:10 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -5,11 +5,12 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("wap", "src/lib.zig");
+    const lib = b.addStaticLibrary("wasmparser", "src/lib.zig");
     lib.setBuildMode(mode);
     lib.install();
 
-    var main_tests = b.addTest("src/lib.zig");
+    var main_tests = b.addTest("src/test.zig");
+    main_tests.addPackagePath("wasmparser", "src/lib.zig");
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
